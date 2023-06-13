bfa410b3ab511755af244448048dcc1140c615c5
Marcel Suter <marcel.suter@bzz.ch>
2023-06-13 09:13:58 +0200

    reading custom field-ids from database

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023061303;
+$plugin->version = 2023061304;
 $plugin->requires = 2015111000;
 $plugin->component = 'local_assignment_export';
 $plugin->dependencies = array(
